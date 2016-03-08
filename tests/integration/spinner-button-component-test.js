5f5545b84c8f5d4498a39c12a38be931097098c2
R.S. Schermer <roland0507@gmail.com>
2016-03-08 12:45:47 +0100

    Migrate unit tests to component integration test

---
+++ tests/integration/spinner-button-component-test.js
@@ -0,0 +1,40 @@
+import hbs from 'htmlbars-inline-precompile';
+import { test, moduleForComponent } from 'ember-qunit';
+
+moduleForComponent('spinner-button', 'SpinnerButtonComponent', {
+  integration: true
+});
+
+test('does not display a spinner when isSpinning is false', function (assert) {
+  this.render(hbs`
+    {{#spinner-button isSpinning=false}}
+      Click me
+    {{/spinner-button}}
+  `);
+
+  assert.equal(this.$('.spin-spinner').length, 0);
+});
+
+test('does display a spinner when isSpinning is true', function (assert) {
+  this.render(hbs`
+    {{#spinner-button isSpinning=true}}
+      Click me
+    {{/spinner-button}}
+  `);
+
+  assert.equal(this.$('.spin-spinner').length, 1);
+});
+
+test('sends the action when clicked when isSpinning is false', function (assert) {
+  this.render(hbs`
+    {{#spinner-button id="my_button" action='dummyAction' isSpinning=false}}
+      Click me
+    {{/spinner-button}}
+  `);
+
+  this.set('actions.dummyAction', () => {
+    assert.ok(true, 'external Action was called!');
+  });
+
+  this.$('#my_button').click();
+});
